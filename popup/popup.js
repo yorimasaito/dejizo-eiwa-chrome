e1b9eb382f550ed9dea25d5e5f29616e3de84e99
tanishiking <rikiriki1238@gmail.com>
2015-10-13 09:58:21 +0900

    Refactor to use callback functions

--- popup/popup.js
+++ popup/popup.js
@@ -2,42 +2,47 @@ var resultDisplay = null;
 var searchWord;
 var MAX_RESULT_WORDS = 5;
 
-function showResultBodies(xmlData) {
+function getBodyFromId(itemId, callback) {
+    var xhr = new XMLHttpRequest();
 
-    function showBodyFromId(itemId) {
-        var xhr = new XMLHttpRequest();
+    itemId = encodeURIComponent(itemId);
+    var params = 'Dic=EJdict' +
+                 '&Item=' + itemId +
+                 '&Loc=' +
+                 '&Prof=XHTML';
 
-        itemId = encodeURIComponent(itemId);
-        var params = 'Dic=EJdict' +
-                     '&Item=' + itemId +
-                     '&Loc=' +
-                     '&Prof=XHTML';
+    var getUrl = 'http://public.dejizo.jp/NetDicV09.asmx/GetDicItemLite';
+    xhr.open('GET', getUrl + '?' + params, true);
+    xhr.onreadystatechange = function() {
+        // if the request completed
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var dom = xhr.responseXML;
+            var head = dom.getElementsByTagName('Head');
+            var body = dom.getElementsByTagName('Body');
+            var res = head[0].innerHTML + body[0].innerHTML;
+            callback(res);
+        } else {
+            callback(xhr.statusText);
+        }
+    };
+    xhr.send(null);
+}
 
-        var getUrl = 'http://public.dejizo.jp/NetDicV09.asmx/GetDicItemLite';
-        xhr.open('GET', getUrl + '?' + params, true);
-        xhr.onreadystatechange = function() {
-            // if the request completed
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                var dom = xhr.responseXML;
-                var head = dom.getElementsByTagName('Head');
-                var body = dom.getElementsByTagName('Body');
-                var res = head[0].innerHTML + body[0].innerHTML;
-                resultDisplay.innerHTML += res;
-            } else {
-                return xhr.statusText;
-            }
-        };
-        xhr.send(null);
-    }
-
+function getResultBodies(xmlData, showResult) {
     var ids = xmlData.getElementsByTagName('ItemID');
-    if (ids.length == 0) {
+    var ids_length = ids.length;
+    if (ids_length == 0) {
         var message = searchWord + ' に一致する情報は見つかりませんでした.';
-        resultDisplay.innerHTML = message;
+        showResult(message);
+    } else if (ids_length == 1){
+        getBodyFromId(ids[0].childNodes[0].nodeValue, function(resHTML){
+            showResult(resHTML);
+        });
     } else {
-        var resultBodies = [];
-        for (var i = 0; i < ids.length; i++) {
-            showBodyFromId(ids[i].childNodes[0].nodeValue);
+        for(var i = 0; i < ids_length; i++) {
+            getBodyFromId(ids[i].childNodes[0].nodeValue, function(resHTML){
+                showResult(resHTML);
+            });
         }
     }
 }
@@ -50,7 +55,6 @@ function lookupWord() {
     searchWord = encodeURIComponent(document.getElementById('searchWordForm').value);
 
     var getUrl = 'http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite';
-
     var params = 'Dic=EJdict&' +
                  'Word=' + searchWord +
                  '&Scope=HEADWORD' +
@@ -66,13 +70,14 @@ function lookupWord() {
         if (xhr.readyState == 4) {
             resultDisplay.innerHTML = '';
             if (xhr.status == 200) {
-                showResultBodies(xhr.responseXML);
+                getResultBodies(xhr.responseXML, function(responseHTML){
+                    resultDisplay.innerHTML += responseHTML;
+                });
             } else {
                 resultDisplay.innerHTML = xhr.statusText;
             }
         }
     };
-
     xhr.send(null);
 }
 
